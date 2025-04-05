2f296671a355336e3c8d725a2836b494ca00e9c3
dengyw144@gmail.com <dengyw144@gmail.com>
2025-04-04 23:34:48 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mylib',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Your Name',
+    description='My awesome reusable library',
+)
