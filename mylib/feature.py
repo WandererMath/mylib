7a651419180fd26e4e8b5011c4a7c06f2ddc8cd5
Wanderer <dengyw144@pitzer-rw01.ten.osc.edu>
2025-06-23 13:10:54 -0400

    init.py update

--- mylib/feature.py
+++ mylib/feature.py
@@ -9,14 +9,16 @@ class Feature:
     name: str
     counts: list    # int
     gene_ids: list # string
+    gene_count: dict
     def __init__(self, path, ribo_seq=False):
         df = pd.read_csv(path, sep="\t", comment="#")
         FIELD_COUNTS=df.columns.tolist()[-1]
         self.counts=df[FIELD_COUNTS].tolist()
         self.gene_ids=df['Geneid'].tolist()
         self.name=os.path.basename(path).split('.')[0]
-        if ribo_seq:
-            self.name='Ribo-'+self.name
+        self.gene_count = {}
+        for g, c in zip(self.gene_ids, self.counts):
+            self.gene_count[g]=self.counts
     def scatter_plot(self, other, base_path):
         os.system(f'mkdir -p {base_path}')
         plt.scatter(self.counts, other.counts)
